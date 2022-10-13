b9f7cf932b4277253e7bdc37ad80f9c241c2ccf2
Rohit Moni <Rohitmdxb@gmail.com>
2022-10-13 03:13:47 +0000

    Now parsing and reading the markdown meta

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"os"
+
+	"github.com/yuin/goldmark"
+	meta "github.com/yuin/goldmark-meta"
+	"github.com/yuin/goldmark/parser"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Started!")
+
+	source, err := os.ReadFile("./roadmap.md")
+	if err != nil {
+		panic(err)
+	}
+
+	// fmt.Print(string(data))
+
+	markdown := goldmark.New(
+		goldmark.WithExtensions(
+			meta.Meta,
+		),
+	)
+
+	var buf bytes.Buffer
+	context := parser.NewContext()
+	if err := markdown.Convert([]byte(source), &buf, parser.WithContext(context)); err != nil {
+		panic(err)
+	}
+	metaData := meta.Get(context)
+	title := metaData["title"]
+	fmt.Println(title)
 }
